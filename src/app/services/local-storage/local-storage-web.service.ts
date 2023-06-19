88310e4ac3b79dcf45c7822095f4ac0ec19a4ffa
RafHuardTL <rafael.huard@technolibre.ca>
2023-06-19 14:20:19 -0400

    [UPD] LocalStorage: Updated LocalStorageWebService get method

--- src/app/services/local-storage/local-storage-web.service.ts
+++ src/app/services/local-storage/local-storage-web.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { LocalStorageService } from './local-storage.service';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class LocalStorageWebService extends LocalStorageService {
-	get(key: string): any {
-		return localStorage.getItem(key);
+	get(key: string): Observable<any> {
+		return of(localStorage.getItem(key));
 	}
 	set(key: string, value: any): void {
 		localStorage.setItem(key, value);
