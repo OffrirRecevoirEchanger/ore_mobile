3952369acc0616717570b127db72eeb1a897d471
RafHuardTL <rafael.huard@technolibre.ca>
2023-06-19 14:18:36 -0400

    [ADD] HomeComponent: Added profile picture element

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+	@ViewChild('profilePicture') profilePicture!: ElementRef;
+
+	ngAfterViewInit() {
+		this.profilePicture.nativeElement;
+	}
+
+	constructor(private authenticationService: AuthenticationService) {
+		this.authenticationService.user$.subscribe((user) => {
+			console.log(user.name);
+			const cleanByteArray = user.image.slice(2, -3);
+			const dataUrl = `data:image/png;base64,${cleanByteArray}`;
+			this.profilePicture.nativeElement.src = dataUrl;
+		});
+	}
+}
