527cef0bd911c4a8bf258b550be976139c926037
RafHuardTL <rafael.huard@technolibre.ca>
2023-06-21 08:23:41 -0400

    [ADD] LoginComponent: Created login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+	loginFormGroup = new FormGroup({
+		username: new FormControl(''),
+		password: new FormControl(''),
+	});
+
+	get username() {
+		return this.loginFormGroup.get('username')?.value;
+	}
+
+	get password() {
+		return this.loginFormGroup.get('password')?.value;
+	}
+
+	login() {
+		console.log('Submitted');
+	}
+}
