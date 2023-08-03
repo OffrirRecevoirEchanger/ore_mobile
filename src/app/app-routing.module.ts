21cc11fdee1551ed14887f21b3d1b6a9264081fe
RafHuardTL <rafael.huard@technolibre.ca>
2023-08-03 10:05:11 -0400

    [UPD] Chat: Replaced ChatMenuComponent with MessagesComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MessagesComponent } from './messages/messages.component';
 
 const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'web/login', component: LoginComponent },
+	{ path: 'messages', component: MessagesComponent },
 	{ path: '', redirectTo: '/', pathMatch: 'full' },
 ];
 
