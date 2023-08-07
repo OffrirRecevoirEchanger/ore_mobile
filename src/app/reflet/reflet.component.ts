9b21a69201635a592e3d57b02d72962af2513f47
RafHuardTL <rafael.huard@technolibre.ca>
2023-08-07 16:24:19 -0400

    [UPD] RefletComponent: Added basic elements and styles

--- src/app/reflet/reflet.component.ts
+++ src/app/reflet/reflet.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {
+	AfterViewInit,
+	Component,
+	ElementRef,
+	HostListener,
+	ViewChild,
+} from '@angular/core';
+import { NavigationComponent } from '../navigation/navigation.component';
 
 @Component({
-  selector: 'app-reflet',
-  templateUrl: './reflet.component.html',
-  styleUrls: ['./reflet.component.scss']
+	selector: 'app-reflet',
+	templateUrl: './reflet.component.html',
+	styleUrls: ['./reflet.component.scss'],
 })
-export class RefletComponent {
+export class RefletComponent implements AfterViewInit {
+	@ViewChild('main') mainElement!: ElementRef<any>;
+	@ViewChild('refletTop') refletTop!: ElementRef<any>;
+	@ViewChild('refletOptions') refletOptions!: ElementRef<any>;
+	@ViewChild('refletContent') refletContent!: ElementRef<any>;
 
+	@HostListener('window:resize', ['$event'])
+	onResize(_event: any) {
+		this.refletContent.nativeElement.style.maxHeight = `${this.getRefletContentHeight()}px`;
+	}
+
+	getRefletContentHeight(): number {
+		return (
+			this.mainElement?.nativeElement.getBoundingClientRect().height -
+			(this.refletTop?.nativeElement.getBoundingClientRect().height +
+				this.refletOptions?.nativeElement.getBoundingClientRect()
+					.height)
+		);
+	}
+
+	ngAfterViewInit() {
+		console.log(this.refletContent.nativeElement);
+		this.refletContent.nativeElement.style.maxHeight = `${this.getRefletContentHeight()}px`;
+	}
 }
